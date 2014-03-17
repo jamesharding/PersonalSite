475498f9ca5e69af9fa97aa2b0e9b6d52c406ef5
James Harding <james.harding.uk@gmail.com>
2014-03-17 18:50:35 -0400

    New post.

--- startup.py
+++ startup.py
@@ -28,37 +28,37 @@ def pygments_css():
 
 @app.route("/")
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route("/engineer/")
 def engineer():
-	return render_template('engineer.html')
+    return render_template('engineer.html')
 
 @app.route("/pilot/")
 def aviation():
-	return render_template('pilot.html')
+    return render_template('pilot.html')
 
 @app.route("/hobbies/")
 def hobbies():
-	return render_template('hobbies.html')
+    return render_template('hobbies.html')
 
 @app.route("/projects/")
 def projects():
-	return render_template('projects.html')
+    return render_template('projects.html')
 
 @app.route("/resume/")
 def resume():
-	return render_template('resume.html')
+    return render_template('resume.html')
 
 @app.route("/work-experience/")
 def workexperience():
-	return render_template('workexperience.html')
+    return render_template('workexperience.html')
 
 @app.route("/posts/")
 def posts():
-	posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
-	posts.sort(key=lambda item:item['date'], reverse=False)
-	return render_template('posts.html', posts=posts)
+    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    posts.sort(key=lambda item: item['date'], reverse=True)
+    return render_template('posts.html', posts=posts)
 
 @app.route('/posts/<name>/')
 def post(name):
@@ -68,15 +68,15 @@ def post(name):
 
 @app.route('/robots.txt')
 def robots():
-	return render_template('robots.txt')
+    return render_template('robots.txt')
 
 @app.route('/sitemap.xml')
 def sitemap():
-	today = date.today()
-	recently = date(year=today.year, month=today.month, day=1)
-	posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
-	posts.sort(key=lambda item:item['date'], reverse=False)
-	return render_template('sitemap.xml', posts=posts, today=today, recently=recently)
+    today = date.today()
+    recently = date(year=today.year, month=today.month, day=1)
+    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    posts.sort(key=lambda item: item['date'], reverse=True)
+    return render_template('sitemap.xml', posts=posts, today=today, recently=recently)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -86,4 +86,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
     else:
-        app.run(host='0.0.0.0', debug=True)
+        app.run(host='0.0.0.0', port=8888, debug=True)
